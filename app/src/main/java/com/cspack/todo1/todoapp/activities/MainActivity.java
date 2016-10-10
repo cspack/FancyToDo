d15777367e7ff7fd15672b7e7b3dce7594b75342
chris <chris.spack@gmail.com>
2016-10-09 18:59:04 -0700

    Refactor package names

--- app/src/main/java/com/cspack/todo1/todoapp/activities/MainActivity.java
+++ app/src/main/java/com/cspack/todo1/todoapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.cspack.todo1.todoapp;
+package com.cspack.todo1.todoapp.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +11,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.cspack.todo1.todoapp.R;
+import com.cspack.todo1.todoapp.TodoNoteAdapter;
+import com.cspack.todo1.todoapp.db.TodoNoteDatabase;
+import com.cspack.todo1.todoapp.fragments.EditDialogFragment;
+import com.cspack.todo1.todoapp.models.TodoNote;
+
 /*
    Simple implementation of ToDo app main activity.
    Adds a very weak checkbox implementation using files.
@@ -18,11 +24,68 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity implements EditDialogFragment.EditDialogListener {
     private static final String TAG = "MainActivity";
-    private TodoNoteDatabase mNoteDb;
     TodoNoteAdapter itemAdapter;
     RecyclerView rvItems;
     LinearLayoutManager itemsLayoutManager;
     EditText newItem;
+    private TodoNoteDatabase mNoteDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup Data.
+        mNoteDb = TodoNoteDatabase.getInstance(getApplicationContext());
+        // Initially populate notes.
+        mNoteDb.refreshNotes();
+
+        itemAdapter = new TodoNoteAdapter(MainActivity.this, mNoteDb.noteList);
+        itemsLayoutManager = new LinearLayoutManager(this);
+
+        // Setup UI Elements.
+        newItem = (EditText) findViewById(R.id.etNewItem);
+
+        rvItems = (RecyclerView) findViewById(R.id.rvItems);
+        rvItems.setHasFixedSize(true);
+        itemsLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvItems.setAdapter(itemAdapter);
+        rvItems.setLayoutManager(itemsLayoutManager);
+
+        // Add swipe handler for deletion.
+        ItemTouchHelper touchHelper = new ItemTouchHelper(new TodoNoteAdapterCallback());
+        touchHelper.attachToRecyclerView(rvItems);
+
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.btnAddItem);
+        addButton.setImageResource(android.R.drawable.ic_input_add);
+    }
+
+    public void handleAddItem(View view) {
+        String itemText = newItem.getText().toString();
+
+        if (itemText.isEmpty()) {
+            Toast.makeText(getApplicationContext(), R.string.note_text_missing, Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        mNoteDb.insertNote(new TodoNote(/*unused id=*/-1, /*completed=*/false,
+                itemText, TodoNote.Priority.MEDIUM));
+        newItem.setText("");
+
+        // Scroll to the bottom.
+        rvItems.smoothScrollToPosition(itemAdapter.getItemCount());
+    }
+
+    @Override
+    public void onFinishEditNote(int position, TodoNote note) {
+        if (position < 0 || position >= mNoteDb.noteList.size()) {
+            Log.e(TAG, "Invalid position sent for edit item: " + position);
+            return;
+        }
+        Log.d(TAG, "Updating note position " + position);
+        mNoteDb.updateNote(note);
+    }
 
     public class TodoNoteAdapterCallback extends ItemTouchHelper.Callback {
         @Override
@@ -59,62 +122,5 @@ public class MainActivity extends AppCompatActivity implements EditDialogFragmen
             mNoteDb.deleteNote(noteHolder.getNote());
         }
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Setup Data.
-        mNoteDb = TodoNoteDatabase.getInstance(getApplicationContext());
-        // Initially populate notes.
-        mNoteDb.refreshNotes();
-
-        itemAdapter = new TodoNoteAdapter(MainActivity.this, mNoteDb.noteList);
-        itemsLayoutManager = new LinearLayoutManager(this);
-
-        // Setup UI Elements.
-        newItem = (EditText) findViewById(R.id.etNewItem);
-
-        rvItems = (RecyclerView) findViewById(R.id.rvItems);
-        rvItems.setHasFixedSize(true);
-        itemsLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        rvItems.setAdapter(itemAdapter);
-        rvItems.setLayoutManager(itemsLayoutManager);
-
-        // Add swipe handler for deletion.
-        ItemTouchHelper touchHelper = new ItemTouchHelper(new TodoNoteAdapterCallback());
-        touchHelper.attachToRecyclerView(rvItems);
-
-        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.btnAddItem);
-        addButton.setImageResource(R.drawable.ic_add_white_24dp);
-    }
-
-    public void handleAddItem(View view) {
-        String itemText = newItem.getText().toString();
-
-        if (itemText.isEmpty()) {
-            Toast.makeText(getApplicationContext(), R.string.note_text_missing, Toast.LENGTH_SHORT)
-                    .show();
-            return;
-        }
-
-        mNoteDb.insertNote(new TodoNote(/*unused id=*/-1, /*completed=*/false,
-                itemText, TodoNote.Priority.MEDIUM));
-        newItem.setText("");
-
-        // Scroll to the bottom.
-        rvItems.smoothScrollToPosition(itemAdapter.getItemCount());
-    }
-
-    @Override
-    public void onFinishEditNote(int position, TodoNote note) {
-        if (position < 0 || position >= mNoteDb.noteList.size()) {
-            Log.e(TAG, "Invalid position sent for edit item: " + position);
-            return;
-        }
-        Log.d(TAG, "Updating note position " + position);
-        mNoteDb.updateNote(note);
     }
 }
